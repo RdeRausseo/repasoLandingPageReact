abb067b3f042b922d5b35116f1eb92a52fe4cb42
RdeRausseo <amrausseo20@gmail.com>
2025-07-15 00:16:55 +0000

    inicializando repositorio para repasar react lading page

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
 				<img src={rigoImage} />
 			</p>
 			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
+				inicializando repositorio
 			</a>
 			<p>
 				Made by{" "}
