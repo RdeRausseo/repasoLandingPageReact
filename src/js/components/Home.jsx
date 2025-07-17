2c82599e23f4797698c64b03ce08386bf6915c7b
RdeRausseo <amrausseo20@gmail.com>
2025-07-17 21:51:58 +0000

    NavBar responsive realizada

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,12 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				inicializando repositorio
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return <Navbar />;
 };
 
 export default Home;
