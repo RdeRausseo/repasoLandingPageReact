c62404ab92defcdfd89b9d1c7153d72f490e1a90
RdeRausseo <amrausseo20@gmail.com>
2025-07-17 22:35:24 +0000

    jumbotron implementado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Jumbotron />
+    </>
+  );
 };
 
 export default Home;
---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <div className="container p-5 bg-light rounded-3">
+      <h1 className="display-1">A Warm Welcome</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti esse
+        porro quas cumque autem, accusantium dicta reprehenderit veniam nam
+        reiciendis rem ea minima? Molestiae itaque repudiandae deserunt mollitia
+        pariatur, dolorem in dolor at atque doloremque!
+      </p>
+      <button className="btn btn-primary btn-lg"> call to action</button>
+    </div>
+  );
+};
+
+export default Jumbotron;
