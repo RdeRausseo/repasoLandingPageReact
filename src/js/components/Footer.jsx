bb71a77f66266c716889c6ca9ba4bf6110fdb9a6
RdeRausseo <amrausseo20@gmail.com>
2025-07-17 23:53:43 +0000

    footer implementado y componente home responsive, se han corregido todos los detalles

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="container-fluid bg-dark text-center">
+      <h5 className="h5 text-white">RdeRausseo #Programmer15</h5>
+    </div>
+  );
+};
+
+export default Footer;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import Jumbotron from "./Jumbotron";
 import Card from "./Card";
+import Footer from "./Footer";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -9,16 +10,20 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <Jumbotron />
-      <div className=" container d-flex gap-2">
-        <Card />
-        <Card />
-        <Card />
+      <div className="flex-grow-1">
+        <Jumbotron />
+        <div className="container d-flex gap-2">
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
-    </>
-  );
+      <Footer className="mt-auto bg-dark text-center" />
+    </div>
+
+  )
 };
 
 export default Home;
